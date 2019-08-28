2d3846fa86e039ad68ae2967ae6dc78a502329a9
Mohammad Shawky <midoshawky100@gmail.com>
2019-08-28 17:50:14 +0200

    Brochure Flutter-App student managment
    this app is like a social media app just for students of alexandria universty
    students and the professiors can easly communicate with each other on it and keep up with the updates in thier college

---
+++ brochure/lib/userData.dart
@@ -0,0 +1,5 @@
+class userData
+{
+  String Name,NID,College,Grades,Section,Notification,ImgUrl;
+  userData(this.Name,this.NID,this.College,this.Grades,this.Section,this.Notification,this.ImgUrl);
+}
