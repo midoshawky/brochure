9f7a487c5be32b48c0696da332d1122e12064e8b
Mohammad Shawky <midoshawky100@gmail.com>
2019-08-28 17:53:19 +0200

    Revert "Brochure Flutter-App student managment"
    This reverts commit 2d3846fa86e039ad68ae2967ae6dc78a502329a9.

--- brochure/lib/userData.dart
+++
@@ -1,5 +0,0 @@
-class userData
-{
-  String Name,NID,College,Grades,Section,Notification,ImgUrl;
-  userData(this.Name,this.NID,this.College,this.Grades,this.Section,this.Notification,this.ImgUrl);
-}
